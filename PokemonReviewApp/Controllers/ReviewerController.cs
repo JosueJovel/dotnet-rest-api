255a51e84352774dabd367ad64b0e9ed65f00f18
Josue Jovel <josuejovel1234@gmail.com>
2024-12-29 02:22:13 -0600

    Implemented endpoint support for Reviewer entity.
    The top to bottom integration for Reviewers include: New repostiory interface/ impl for reviewers, new reviewer DTO, new reviewer Controller, new reviewer-reviewerDto mapping via Automapper, and of course not forgetting to register our reviewerRepository as a service in our Program.Cs file for dependency injection.

---
+++ PokemonReviewApp/Controllers/ReviewerController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")] //Annotations; @ Equivalent from spring boot
+    [ApiController]
+    public class ReviewerController : Controller
+    {
+        private readonly IReviewerRepository _reviewerRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewerController(IReviewerRepository reviewerRepository, IMapper mapper)
+        {
+            this._reviewerRepository = reviewerRepository;
+            this._mapper = mapper;
+        }
+
+        //CONTROLLER ENDPOINT
+        [HttpGet]//Http Get Annotation
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Reviewer>))] //Annotation determing response type
+        public IActionResult GetReviewers() //Controller endpoint method
+        {
+            var reviewers = _mapper.Map<List<ReviewerDto>>(_reviewerRepository.GetReviewers());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(reviewers);
+        }
+
+        [HttpGet("{reviewerId}")]
+        [ProducesResponseType(200, Type = typeof(Reviewer))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewer(int reviewerId)
+        {
+            if(!_reviewerRepository.ReviewerExists(reviewerId)) return NotFound();
+            var reviewer = _mapper.Map<ReviewerDto>(_reviewerRepository.GetReviewer(reviewerId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(reviewer);
+        }
+
+        [HttpGet("{reviewerId}/reviews")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewsByReviewer(int reviewerId)
+        {
+            if (!_reviewerRepository.ReviewerExists(reviewerId)) return NotFound();
+            var reviewerReviews = _mapper.Map<List<ReviewDto>>(_reviewerRepository.GetReviewsByReviewer(reviewerId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(reviewerReviews);
+        }
+    }
+}
---
+++ PokemonReviewApp/Repository/ReviewerRepository.cs
@@ -0,0 +1,35 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public ReviewerRepository(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _dataContext.Reviewers.Where(r => r.Id == reviewerId).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _dataContext.Reviewers.ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _dataContext.Reviewers.Where(r => r.Id == reviewerId).SelectMany(r => r.Reviews).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _dataContext.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
